64f00c44408a9a8c5e4e8b452cdfa5f0b58b4106
Yến Nhi <huynhthiyennhi12042006@gmail.com>
2026-04-07 20:07:58 +0700

    fix: sync prisma exports and imports project-wide

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { prisma } from "@/lib/prisma";
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export default prisma;
